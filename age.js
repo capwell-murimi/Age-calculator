b28b745f875e1724defc7059f2afb5fc58e8ee18
Capwell Murimi <85755317+capwell-murimi@users.noreply.github.com>
2024-01-16 01:44:41 +0300

    Update age.js

--- age.js
+++ age.js
@@ -1,8 +1,8 @@
 //challenge 1: Age in days
 
 function ageInDays(){
-var birthYear = prompt('this will calculate your age good friend...')
-var ageInDayss = (2022-birthYear);
+var birthYear = prompt('Enter your birth year my good friend...')
+var ageInDayss = (2024-birthYear) * 365;
 var h1 = document.createElement('h1');
 var textAnswer = document.createTextNode(' You are ' + ageInDayss + ' days old ')
 h1.setAttribute('id', 'ageInDays');
